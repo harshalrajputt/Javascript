d1a47a3890858abb3053b14acc05eb4fa21e72ff
amol112225 <amolkendre18@gmail.com>
2026-01-04 10:19:17 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as never js version
+// alert("Hello") we are using nodejs, not browser
+
+/*
+Data types
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+null =>standalone value
+undefined=>not defined yet
+symbol=>unnique
+object
+
+*/
+let age=19
+console.log(typeof(age)); // number
+console.log(typeof(null)); // object
+console.log(typeof(undefined)); // undefined
+
+
+
+
