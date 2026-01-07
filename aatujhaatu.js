a1bde8e9ec964ef73bb35aaabb11707a4303740a
harshalrajputt <rajputharshal2004@gmail.com>
2026-01-07 12:10:43 +0530

    first push by harshal bhau

---
+++ aatujhaatu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const aatujhaatu = () => {
+  return (
+    <div>
+      <h1>
+        aaja..........
+        hooo aajaaa...................
+        ajaaa......... hoo............ aaaaaaajaaaaaaaaaaaa.
+        
+      </h1>
+    </div>
+  )
+}
+
+export default aatujhaatu
